e1d638619facf069bbe08ac475a2b7d9adf45cd2
Shannon Francis Retuerma <snretuerma@up.edu.ph>
2020-10-02 16:40:42 +0800

    Initial content for constants.js (#2)
    * Initial content for constants.js

---
+++ resources/js/constants.js
@@ -0,0 +1,13 @@
+export const DOCUMENT_TYPE = [];
+
+export const DOCUMENT_PURPOSE = [];
+
+export const TRACKING_ACTIONS = [];
+
+export const TRACKING_STATUS = [];
+
+export const LOG_ACTIONS = [
+    'add',
+    'edit',
+    'delete'
+];
